325687db1351fce63d2f3d77bd8a959c4d63c1ea
adolf fick <dukelovejaz@gmail.com>
2021-11-15 16:54:33 +0300

    add navigation drawer activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     TextView digit;
     ImageView chelick;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +29,53 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
     }
 
+    /**
+     *
+     * @param baseDigit
+     * @param smallDigit
+     * @return module of both
+     */
     private int moduleDigit(int baseDigit, int smallDigit) {
         return baseDigit % smallDigit;
     }
 
+    /**
+     *
+     * @param view
+     */
     public void moduleCounter(View view) {
-        int bD = 100;
+        int bD = new Random().nextInt(61) + 20;
+        ;
         int sD = 3;
         int res = moduleDigit(bD, sD);
-        digit.setText(""+res);
-        chelick.setVisibility(View.VISIBLE);
+        digit.setText("" + res);
+
+        if (res == 1) {
+            Intent i = new Intent(MainActivity.this, NavActivity.class);
+            startActivity(i);
+        }
+//        switch (res) {
+//            case 0:
+//                chelick.setVisibility(View.VISIBLE);
+//                break;
+//            case 1:
+//                chelick.setVisibility(View.INVISIBLE);
+//                break;
+//            case 2:
+//                digit.setTextColor(Color.GREEN);
+//                break;
+//            default:
+//                break;
+//        }
+//
+//        if (res == 0) {
+//            chelick.setVisibility(View.VISIBLE);
+//        } else if(res == 1) {
+//            chelick.setVisibility(View.INVISIBLE);
+//        }else if(res == 2) {
+//            digit.setTextColor(Color.GREEN);
+//        }else{
+//            Log.e("LOL", " KEK");
+//        }
     }
 }
